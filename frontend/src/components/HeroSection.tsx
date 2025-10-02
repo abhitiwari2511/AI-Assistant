9e62f037a97c1c77495e726f4626fde8857cccee
abhitiwari2511 <abhishek.tiwari@hmritm.ac.in>
2025-10-02 19:08:30 +0530

    added home page

--- frontend/src/components/HeroSection.tsx
+++ frontend/src/components/HeroSection.tsx
@@ -1,12 +1,55 @@
+import { useNavigate } from "react-router-dom";
 import Navbar from "./Navbar";
+import { Button } from "./ui/button";
 
 const HeroSection = () => {
+  const navigate = useNavigate()
+
+  const handleClick = () => {
+    navigate("/signin")
+  }
+
   return (
     <div className="h-screen relative">
       <Navbar />
       <div className="h-full flex items-center justify-center">
-        <div className="max-w-[38rem] mx-auto text-5xl text-white text-center">
-          
+        <div className="max-w-[38rem] mt-10 mx-auto text-white text-center">
+          <Button
+            variant={"outline"}
+            className="mb-4 h-8 bg-zinc-800/40 rounded-full"
+          >
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              width="24"
+              height="24"
+              viewBox="0 0 24 24"
+              fill="none"
+              stroke="currentColor"
+              stroke-width="2"
+              stroke-linecap="round"
+              stroke-linejoin="round"
+              className="lucide lucide-bot-icon lucide-bot"
+            >
+              <path d="M12 8V4H8" />
+              <rect width="16" height="12" x="4" y="8" rx="2" />
+              <path d="M2 14h2" />
+              <path d="M20 14h2" />
+              <path d="M15 13v2" />
+              <path d="M9 13v2" />
+            </svg>
+            Introducing AI Assistant
+          </Button>
+          <h1 className="font-bold max-w-[30rem] text-white w-full text-[2rem]">
+            Welcome to the world of an AI The Future is here !!
+          </h1>
+
+          <Button
+            variant={"outline"}
+            className="text-black mt-10 cursor-pointer hover:bg-blue-300"
+            onClick={handleClick}
+          >
+            Get Started
+          </Button>
         </div>
       </div>
     </div>
--- frontend/src/components/Navbar.tsx
+++ frontend/src/components/Navbar.tsx
@@ -1,15 +1,34 @@
+import { useNavigate } from "react-router-dom";
+
+const navItems = [
+  { label: "Home", route: "/" },
+  { label: "Register", route: "/signup" },
+];
+
 const Navbar = () => {
+  const navigate = useNavigate();
+
   return (
     <nav className="absolute rounded-full bg-zinc-950/60 border-1 border-zinc-800 left-1/2 m-8 transform -translate-x-1/2 min-w-[38rem] mx-auto z-30">
       <div className="flex items-center justify-between p-4">
-        <div className="text-lg text-white ml-2 font-bold">AI Assistant</div>
+        <div className="flex justify-center">
+          <img
+            src="https://imgs.search.brave.com/AIOosMEPLEri9dQiYOgso-64B96r9VncHzH666CPM3U/rs:fit:860:0:0:0/g:ce/aHR0cHM6Ly9jZG4u/cGl4YWJheS5jb20v/cGhvdG8vMjAyMy8w/Mi8wNS8yMC8wMS9h/aS1nZW5lcmF0ZWQt/Nzc3MDQ3NF8xMjgw/LnBuZw"
+            alt="logo"
+            className="h-8 w-8"
+          />
+          <div className="text-lg text-white ml-2 font-bold">AI Assistant</div>
+        </div>
         <div className="flex gap-4 mr-2">
-          <button className="text-cyan-500 text-sm cursor-pointer hover:text-cyan-500 font-semibold transition-colors">
-            Home
-          </button>
-          <button className="text-cyan-500 text-sm cursor-pointer hover:text-cyan-500 font-semibold transition-colors">
-            Login
-          </button>
+          {navItems.map((item, index) => (
+            <button
+              key={index}
+              onClick={() => navigate(item.route)}
+              className="text-cyan-500 text-sm cursor-pointer hover:text-cyan-400 font-bold transition-colors"
+            >
+              {item.label}
+            </button>
+          ))}
         </div>
       </div>
     </nav>
