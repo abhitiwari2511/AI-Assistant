7f1f9a9846e6017c1df11b91f76317a27df8408f
abhitiwari2511 <abhishek.tiwari@hmritm.ac.in>
2025-10-04 00:42:41 +0530

    fix

--- backend/src/app.ts
+++ backend/src/app.ts
@@ -6,16 +6,20 @@ const app = express();
 app.set("trust proxy", 1);
 
 const corsOptions = {
-    origin: process.env.FRONTEND_URL,
-    credentials: true,
-    methods: 'GET,HEAD,PUT,PATCH,POST,DELETE,OPTIONS',
-    allowedHeaders: ['Content-Type', 'Authorization', 'Origin', 'X-Requested-With', 'Accept'],
+  origin: process.env.FRONTEND_URL,
+  credentials: true,
+  methods: "GET,HEAD,PUT,PATCH,POST,DELETE,OPTIONS",
+  allowedHeaders: [
+    "Content-Type",
+    "Authorization",
+    "Origin",
+    "X-Requested-With",
+    "Accept",
+  ],
 };
+console.log(corsOptions.origin);
 
-app.use(
-  cors(corsOptions)
-);
-app.options('*', cors(corsOptions))
+app.use(cors(corsOptions));
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(cookieParser());
