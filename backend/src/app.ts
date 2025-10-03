4d6cb4d97cfc1642f2b154eefbe4cadf9bc2a26c
abhitiwari2511 <abhishek.tiwari@hmritm.ac.in>
2025-10-04 00:34:57 +0530

    added fix

--- backend/src/app.ts
+++ backend/src/app.ts
@@ -1,12 +1,7 @@
-import dotenv from "dotenv";
 import express from "express";
 import cors from "cors";
 import cookieParser from "cookie-parser";
 const app = express();
-
-dotenv.config({
-  path: "./.env",
-});
 
 app.set("trust proxy", 1);
 
