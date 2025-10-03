9a5b6e6f4dad669724285fed7354d0318b1f2973
abhitiwari2511 <abhishek.tiwari@hmritm.ac.in>
2025-10-04 00:30:54 +0530

    added headers

--- backend/src/app.ts
+++ backend/src/app.ts
@@ -1,16 +1,26 @@
+import dotenv from "dotenv";
 import express from "express";
 import cors from "cors";
 import cookieParser from "cookie-parser";
 const app = express();
 
+dotenv.config({
+  path: "./.env",
+});
+
 app.set("trust proxy", 1);
 
-app.use(
-  cors({
+const corsOptions = {
     origin: process.env.FRONTEND_URL,
     credentials: true,
-  })
+    methods: 'GET,HEAD,PUT,PATCH,POST,DELETE,OPTIONS',
+    allowedHeaders: ['Content-Type', 'Authorization', 'Origin', 'X-Requested-With', 'Accept'],
+};
+
+app.use(
+  cors(corsOptions)
 );
+app.options('*', cors(corsOptions))
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(cookieParser());
